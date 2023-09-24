30ac718816959d0119bc18bde248c60869bc560e
yangyuan917 <youremail@example.com>
2023-09-24 17:17:06 +0800

    V1.0.0

--- app_holding.py
+++ app_holding.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask
 from flask_cors import CORS
 # from flask_socketio import SocketIO
@@ -17,6 +18,13 @@ def show_macro():
     res = query_table(f"select * from ads_asset_concentrate", SOURCE).get_df()
     # res = res.fillna(0)
     return dict(code=200, data=res.to_dict())
+
+
+@app_holding.route('/catergory_list', methods=['GET', 'OPTIONS'])
+def show_catergory_list():
+    res = ['城投债券', '同业存单', '金融债', '利率债', '非标', '同业借款', '债券类公募基金', '混合类公募基金', '股票类公募基金',
+           '股票', '城投abs', 'REITs', '存款', '现金']
+    return dict(code=200, data=res)
 
 
 if __name__ == '__main__':
