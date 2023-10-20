13ed3c6d1e3a182044a410bb1f539bf1ead02364
yangyuan917 <youremail@example.com>
2023-10-20 15:53:08 +0800

    V2.0.5 增加散点图fix bug

--- app_holding.py
+++ app_holding.py
@@ -402,7 +402,7 @@ def show_ads_curve_cnbd():
 
 @app_holding.route('/fixincome_price', methods=['GET', 'OPTIONS'])
 def show_ads_fixincome_price():
-    res = query_table(f"select * from ads_fixincome_price where term >= 30 limit 100", SOURCE).get_df()
+    res = query_table(f"select * from ads_fixincome_price where term >= 0.3 limit 100", SOURCE).get_df()
     return dict(code=200, data=[{'name': type_, 'data': res[res.类别1 == type_][['term', 'yield']].values.tolist(),
                                  'code': res[res.类别1 == type_]['symbol2'].to_list(),
                                  'bond_name': res[res.类别1 == type_]['名称'].to_list()} for type_ in res.类别1])
