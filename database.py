551419b22676ba72b1e8b4af7596da52b9d4ce3e
yangyuan917 <youremail@example.com>
2023-11-17 17:12:05 +0800

    V3.0.1 优化接口和项目

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 
-HOLDING = 'holding'
-REAL_ESTATE = 'real_estate'
-BOND_MARKET = 'bond_market'
-STOCK_MARKET = 'stock_market'
+# HOLDING = 'holding'
+# REAL_ESTATE = 'real_estate'
+# BOND_MARKET = 'bond_market'
+# STOCK_MARKET = 'stock_market'
 
-# HOLDING = 'ads_holding'
-# REAL_ESTATE = 'ads_real_estate'
-# BOND_MARKET = 'ads_bond_market'
-# STOCK_MARKET = 'ads_stock_market'
+HOLDING = 'ads_holding'
+REAL_ESTATE = 'ads_real_estate'
+BOND_MARKET = 'ads_bond_market'
+STOCK_MARKET = 'ads_stock_market'
